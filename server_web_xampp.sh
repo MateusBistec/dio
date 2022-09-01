6db5717b889fbddde184111e3aea346991f42ee4
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2022-09-01 19:31:23 +0000

    configura compartilhamento de pasta com o samba

--- server_web_xampp.sh
+++ server_web_xampp.sh
@@ -18,8 +18,25 @@ chmod 755 xampp-linux-x64-8.1.6-0-installer.run
 echo INSTALANDO XAMPP
 echo 'Y' | ./xampp-linux-x64-8.1.6-0-installer.run
 rm -rf xampp*
+rm -rf /opt/lampp/htdocs/*
+rm -rf /opt/lampp/htdocs/.*
 echo REMOVE INSTALADOR DO XAMPP
 
 #mudando permições da pasta
 chmod 777 -R /opt/lampp/htdocs
 echo  PERMIÇÕES DA PASTA HTDOCS PUBLICA
+
+
+#=======================================================================================================
+#configurando samba no htdocs
+echo ""  >> /etc/samba/smb.conf
+echo "[webserver]" >> /etc/samba/smb.conf
+echo "path = /opt/lampp/htdocs" >>  /etc/samba/smb.conf
+echo "writable = yes" >> /etc/samba/smb.conf
+echo "guest ok = yes" >> /etc/samba/smb.conf
+echo "guest only = yes" >> /etc/samba/smb.conf
+
+echo REINICIANDO SERVER SMB
+service smbd restart
+
+echo "SISTEMA CONFIGURADO!!!"
